29bdebfef712952790b02d3ef14cd0fc03fda452
Stefan Biereigel <stefan@biereigel.de>
2014-02-25 16:29:39 +0100

    add hfmod module and implement mission flow
    this adds a dummy module that will handle the actual modulation and
    encoding for aprs and sstv, also fmmod.
    it also includes the flow that will be in the mission, including
    parameters for mission time.
    some things still need to be done, they are marked with TODO

---
+++ hfmod.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# python wrapper for modulation and encoding methods
+
+def aprs(lat, lon, alt, out_fname):
+	pass
+
+def sstv(in_fname, out_fname):
+	pass
+
+def fmmod(in_fname, out_fname, fc):
+	pass
