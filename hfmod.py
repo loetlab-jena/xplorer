e86c06262cd48b06c373fc91e23662fbca2d63da
Stefan Biereigel <stefan@biereigel.de>
2014-02-26 19:15:42 +0100

    add dummycode to hfmod

--- hfmod.py
+++ hfmod.py
@@ -3,10 +3,17 @@
 # python wrapper for modulation and encoding methods
 
 def aprs(lat, lon, alt, out_fname):
+	# call afsk-encoder with its parameters
+	# resample
+	# fmmod into aprs.wav
 	pass
 
 def sstv(in_fname, out_fname):
+	# call sstv-encoder with its parameters
+	# resample
+	# fmmod into out_fname.wav
 	pass
 
 def fmmod(in_fname, out_fname, fc):
+	# call fmmod with its parameters
 	pass
