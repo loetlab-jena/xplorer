f89cfce8eba9602a7085911120f38b7b84c74478
Stefan Biereigel <stefan@biereigel.de>
2014-02-27 11:53:45 +0100

    add the production code to hfmod
    this is untested

--- hfmod.py
+++ hfmod.py
@@ -2,18 +2,23 @@
 #
 # python wrapper for modulation and encoding methods
 
-def aprs(lat, lon, alt, out_fname):
-	# call afsk-encoder with its parameters
-	# resample
-	# fmmod into aprs.wav
-	pass
+import os
 
-def sstv(in_fname, out_fname):
-	# call sstv-encoder with its parameters
-	# resample
-	# fmmod into out_fname.wav
-	pass
+gain = 0.5
 
 def fmmod(in_fname, out_fname, fc):
 	# call fmmod with its parameters
+	os.system('./fmmod %s %s %s 3000 %s' % (in_fname, out_fname, str(fc), str(gain)))
+	pass
+
+def aprs(lat, lon, alt, out_fname):
+	os.system('./afsk-encoder.py %s %s %s aprs_enc.wav' % (str(lat), str(lon), str(alt)))
+	os.system('resample -to 48000 aprs_enc.wav aprs_resamp.wav')
+	fmmod('aprs_resamp.wav', 'aprs_fmmod.wav', 5000)
+	pass
+
+def sstv(in_fname, out_fname):
+	os.system('./robot36 %s sstv_enc.wav' % in_fname)
+	os.system('resample -to 48000 sstv_enc.wav sstv_resamp.wav')
+	fmmod('sstv_resamp.wav', out_fname, 5000)
 	pass
